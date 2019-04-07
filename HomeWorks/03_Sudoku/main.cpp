a740e350cdc64f77e3a75852030263edfdb714b9
bazurita2 <brayanzurita@hotmail.com>
2019-04-07 17:28:41 -0500

    update new functions

--- HomeWorks/03_Sudoku/main.cpp
+++ HomeWorks/03_Sudoku/main.cpp
@@ -14,18 +14,18 @@ int bandera=0,teclado=0,b,c,contadorx=0,contadory=0,xx=5,yy=5,jota,fstop=0,aleat
 int opcion,opcionn,contadoryy,contadorxx;
 
 int main()
-{	
+{
 	mapa=(char**)malloc(9*sizeof(char*));
 	arreglo1=(int**)malloc(9*sizeof(int*));
 	arreglo2=(int**)malloc(9*sizeof(int*));
 	vista2=(int**)malloc(9*sizeof(int*));
-	
+
 	for(int i=0; i<9; i++)
 	{
 		*(mapa+i)=(char*)malloc(9*sizeof(char));
 		*(arreglo1+i)=(int*)malloc(9*sizeof(int));
-		*(arreglo2+i)=(int*)malloc(9*sizeof(int));	
-		*(vista2+i)=(int*)malloc(9*sizeof(int));	
+		*(arreglo2+i)=(int*)malloc(9*sizeof(int));
+		*(vista2+i)=(int*)malloc(9*sizeof(int));
 	}
 	Sudoku sudoku=Sudoku();
 
@@ -80,8 +80,8 @@ int Sudoku::principal()
 							if(fstop==1){pintar(2);}
 							else{pintar(1);}
 							printf("%d",jota);
-							gotoxy(xx,yy);	
-						}	
+							gotoxy(xx,yy);
+						}
 						if(teclado==8 && mapa[contadory][contadorx]=='°')
 						{
 							pintar(3);
@@ -89,13 +89,13 @@ int Sudoku::principal()
 							printf("°");
 							mapa[contadory][contadorx]='°';
 							arreglo2[contadory][contadorx]=0;
-							gotoxy(xx,yy);		
-						}					
+							gotoxy(xx,yy);
+						}
 		}
 		if(teclado==120)
 		{
 			system("CLS");
-			return 0;	
+			return 0;
 		}
 		else
 		{
@@ -104,8 +104,8 @@ int Sudoku::principal()
 		}
 }
 
-int Sudoku::moverx(int teclado,int x) 
-{                                                        
+int Sudoku::moverx(int teclado,int x)
+{
 						if(teclado==97)
 						{
 							x-=6;
@@ -123,7 +123,7 @@ int Sudoku::moverx(int teclado,int x)
 return	x;
 }
 
-int Sudoku::movery(int teclado,int y) 
+int Sudoku::movery(int teclado,int y)
 {
 						if(teclado==119)
 						{
@@ -137,18 +137,18 @@ int Sudoku::movery(int teclado,int y)
 								y+=2;
 								if(y==11){y=13;}
 								else if(y==19){y=21;}
-								else if(y>25){y=5;}	
+								else if(y>25){y=5;}
 						}
 return	y;
 }
 void Sudoku::gotoxy(int x,int y)
 {
-	HANDLE hCon; 
+	HANDLE hCon;
 	hCon=GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD dwPos;
 	dwPos.X=x;
 	dwPos.Y=y;
-	SetConsoleCursorPosition(hCon,dwPos); 
+	SetConsoleCursorPosition(hCon,dwPos);
 }
 void Sudoku::mundo(int opcion,int opcionn)
 {
@@ -173,8 +173,121 @@ for(b=0;b<9;b++)
 				*(*(arreglo2+b)+c)=*(*(arreglo1+b)+c);
 				*(*(mapa+b)+c)='|';
 			}
-			else{printf("%6c",mapa[b][c]);}	
+			else{printf("%6c",mapa[b][c]);}
 		}
 		else*(*(mapa+b)+c)='|'; printf("%6d",*(*(arreglo1+b)+c));}
 	}
-}						
+}
+
+void Sudoku::solucion(int** arreglo1)
+{
+srand(time(NULL));
+do{
+	conta2=0;conta=0;
+	for(b=0;b<9;b++)
+	{
+		for(c=0;c<9;c++)
+		{
+			*(*(arreglo1+b)+c)=0;
+			*(*(vista2+b)+c)=0;
+		}
+	}
+	for(b=0;b<9;b++)
+	{
+		for(c=0;c<9;c++)
+		{
+			conta=0;
+			i=condicion(b,c);
+				do
+				{
+					aleatorio=1+rand()%9;
+					fstop=buscar_num(aleatorio,arreglo1,9,b,c);
+					conta++;
+					if(conta>100)
+					{
+						fstop=0;
+						*(*(vista2+i)+(aleatorio-1))=0;
+						c=9;
+						b=9;
+					}
+				}while(fstop==1 || *(*(vista2+i)+(aleatorio-1))==1 );
+				if(c<3){*(*(vista2+i)+(aleatorio-1))=1;}
+				else if(c>2 && c<6){*(*(vista2+i)+(aleatorio-1))=1;}
+				else if(c>5 && c<9){*(*(vista2+i)+(aleatorio-1))=1;}
+					*(*(arreglo1+b)+c)=aleatorio;
+		    	conta2++;
+		}
+	}
+}while(conta2!=81);
+}
+int Sudoku::buscar_num(int numero,int** arreglo,int tam,int b,int c)
+{
+int i,stop=0;
+	for(i=0;(i<tam && stop==0);i++)
+	{
+		if(	*(*(arreglo+b)+i)==numero || *(*(arreglo+i)+c)==numero){stop=1;}
+	}
+return stop;
+}
+void Sudoku::pintar(int teclado)
+{
+	HANDLE hConsoleOutput;
+	COORD coord;
+	hConsoleOutput = GetStdHandle (STD_OUTPUT_HANDLE);
+	if(teclado==2)
+	{
+		SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),12);
+	}
+	else if(teclado==1)
+	{
+		SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),10);
+	}
+	else if(teclado==120)
+	{
+		SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),15);
+	}
+	else if(teclado==3)
+	{
+		SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),11);
+	}
+}
+void Sudoku::menu()
+{
+	pintar(3);
+	do
+		{system("CLS");
+		printf("Eliga una opcion: \n 1. Automatico \n 2. Manual\n");
+		scanf("%d",&opcion);
+		}while(opcion<1 || opcion>2);
+		if(opcion!=1)
+		{
+			do
+			{system("CLS");
+				printf("Eliga una opcion: \n 1. Facil \n 2. Medio \n 3. Dificil");
+				scanf("%d",&opcionn);
+			}while(opcionn<1||opcionn>3);
+		}
+		system("CLS");
+}
+int Sudoku::condicion(int b,int c)
+{
+			if(b<3)
+			{
+				if(c<3){i=0;}
+				else if(c>2 && c<6){i=1;}
+				else if(c>5 && c<9){i=2;}
+			}
+			else if(b>2 && b<6)
+			{
+				if(c<3){i=3;}
+				else if(c>2 && c<6){i=4;}
+				else if(c>5 && c<9){i=5;}
+			}
+				else if(b>5 && b<9)
+			{
+				if(c<3){i=6;}
+				else if(c>2 && c<6){i=7;}
+				else if(c>5 && c<9){i=8;}
+			}
+return i;
+}
